2b1cb1df4fd304eacee53bae27c68cb3f806b618
Aushah Gowhar <waniaushahgowhar@gmail.com>
2026-04-21 20:05:16 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://snitch-frontend.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 );
